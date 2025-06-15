bd61f436dd84898da6727a668147ff9f3abba328
lois-narh <123365216+lois-narh@users.noreply.github.com>
2025-06-15 17:52:33 +0100

    Add files via upload

--- Valid Parantheses.js
+++ Valid Parantheses.js
@@ -1,1 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = [];
+    const brackets = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
 
+    for (const char of s) {
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else if (char in brackets) {
+            if (stack.length === 0 || stack.pop() !== brackets[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
